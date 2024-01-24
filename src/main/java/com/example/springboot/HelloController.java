20f2134d9aa908cdcbef3112618a916605584824
zanaboni <zanaboni@ttsnetwork.com>
2024-01-24 09:57:18 +0100

    Implemented #1

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -4,12 +4,20 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
 	@GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String hello() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping(value = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, String> health() {
+		return Collections.singletonMap("status", "OK");
 	}
 
 }
